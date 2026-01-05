7cc8c376056846af9c6abf59bd2989302c5a8393
Bohdansnopok <snopokbogdan3@gmail.com>
2026-01-05 16:44:50 +0200

    allow all domains cors

--- index.js
+++ index.js
@@ -7,22 +7,7 @@ const fs = require("fs");
 
 const app = express();
 
-const allowedOrigins = [
-  'https://gayatri-dusky.vercel.app',
-  'http://localhost:3000'
-];
-
-app.use(cors({
-  origin: function(origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      console.log(' Blocked by CORS:', origin);
-      callback(new Error('CORS not allowed'), false);
-    }
-  },
-  credentials: true
-}));
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
